af10c86685c380600899b37138d3557dfacf7cef
achreflouati <93096734+achreflouati@users.noreply.github.com>
2025-08-19 03:50:27 +0200

    Create setup.py
    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='custom_hr',
+    version='0.0.1',
+    description='Custom HR Leave Type App for Frappe',
+    author='Achref Louati',
+    author_email='lsi.louati@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[]
+)
